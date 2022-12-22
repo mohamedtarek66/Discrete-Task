a2a23ce8efd2a8489517f34864df2f857e5752a6
mohamedtarek66 <121250952+mohamedtarek66@users.noreply.github.com>
2022-12-22 19:10:40 +0200

    Add files via upload

---
+++ Discrete1/ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {   
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter the Start Number : ");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the End Number : ");
+            int n2 = int.Parse(Console.ReadLine());
+
+
+            for (int i = n1; i <= n2; i++)
+            {
+                int c = 0;
+                for (int j = 1; j <= i; j++)
+                {
+                    if (j == 1 || i == j)
+                        c++;
+ 
+                    else if (i % j == 0)
+                        break;
+                }
+                if (c == 2)
+                    Console.WriteLine("{0} is Prime Number", i);
+            }
+
+
+        }
+    }
+}
---
+++ Discrete2/Discrete/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Discrete
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter the Start Number : ");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the End Number : ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            for (int i = n1; i < n2; i++)
+            {
+                int sum = 0;
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                        sum += j;
+                }
+                if (sum == 0)
+                    continue;
+                if (i == sum)
+
+                    Console.WriteLine("{0} is Perfect Number", i);
+            }
+
+
+
+        }
+    }
+}
